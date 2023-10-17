415d61bb550fc010834cf5dcb9ea6db3fc4d94c8
hannanhunny01 <hannanhoney5000@gmail.com>
2023-10-16 22:50:38 -0300

    working on schduler with queque

--- controller/crawlerSchduler/crawlerScheduler.js
+++ controller/crawlerSchduler/crawlerScheduler.js
@@ -6,30 +6,39 @@ const { conMainPage } = require('../crawlers/concursoCrawler');
 
 const mainPageScheduler = [pasMainPage, vestMainPage, conMainPage];
 
+const maxRetries = 3;
+
+// Object to store retry counts for each function
+const retryCounts = {};
+
 async function checkMainPageCebraspe() {
-  const maxRetries = 3;
+  functionStack = [...mainPageScheduler]
+  for (let i = 0; i < functionStack.length; ) {
+    const func = functionStack[i];
 
-  for (let i = 0; i < mainPageScheduler.length; i++) {
-    const item = mainPageScheduler[i];
-    let retryCount = 0;
-    let success = false;
-
-    while (retryCount < maxRetries && !success) {
-      success = await item();
-
-      if (!success) {
-        retryCount++;
-      }
+    if (retryCounts[func] === undefined) {
+      retryCounts[func] = 0;
     }
 
-    if (success) {
-      mainPageScheduler.splice(i, 1);
-      i--; 
+    if (retryCounts[func] < maxRetries) {
+      if (await func()) {
+   // If the function returns true, remove it from the stack
+        functionStack.splice(i, 1);
+        delete retryCounts[func];
+      } else {
+   // If the function returns false, move it to the end of the stack
+        const failedFunc = functionStack.splice(i, 1)[0];
+        functionStack.push(failedFunc);
+        retryCounts[failedFunc]++; // Increment the retry count for this function
+      }
     } else {
-      mainPageScheduler.push(mainPageScheduler.splice(i, 1)[0]);
+     // Remove the function if it has reached the maximum retries
+      functionStack.splice(i, 1);
+      delete retryCounts[func];
     }
   }
 }
+
 
 cron.schedule('0 */3 * * *', checkMainPageCebraspe);
 
